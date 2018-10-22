fe72770444921ef1a26014c84e484f10b5c651b8
Xu Junjian <JP23021@w022581809459m.local>
2018-10-22 18:53:09 +0900

    Remove fzf plugin

--- oh-my-zsh/fzf/fzf.plugin.zsh
+++
@@ -1,7 +0,0 @@
-# Setting fd as the default source for fzf
-export FZF_DEFAULT_COMMAND='fd --type f'
-
-# To apply the command to CTRL-T as well
-export FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
-
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh 
